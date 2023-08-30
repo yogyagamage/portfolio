796d4f4e15da6cca32c970ed50ee0891274264a6
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2023-08-30 22:02:11 +0530

    project section updated

--- utils/components/pages/index/projects.tsx
+++ utils/components/pages/index/projects.tsx
@@ -1,24 +1,22 @@
-import { ArrowCircleRight } from '@mui/icons-material';
-import { IconButton, ImageList, ImageListItem, ImageListItemBar, Typography } from '@mui/material';
-import Link from 'next/link';
+import { ArrowCircleRight } from "@mui/icons-material";
+import { IconButton, ImageList, ImageListItem, ImageListItemBar, Typography } from "@mui/material";
+import Link from "next/link";
 import projectConfig from "../../../data/projects.json";
-import CommonDivider from '../../common/commonDivider';
+import CommonDivider from "../../common/commonDivider";
+import SubSectionTitle from "../../common/subSectionTitle";
 
 export default function Projects({ data }) {
     return (
         <>
             <br />
-            <Typography variant="h4" color="text.primary">
-                {data.title}
-            </Typography>
+            <SubSectionTitle data={ data } />
             <br />
 
-            <ImageList cols={3}>
-
+            <ImageList cols={ 3 }>
                 {
-                    projectConfig.projects.slice(0, 3).map((project) =><SingleProject project={project} /> )
+                    projectConfig.projects.slice(0, 3).map(
+                        (project) => <SingleProject key={ project.name } project={ project } /> )
                 }
-
             </ImageList>
 
             <CommonDivider />
@@ -28,23 +26,23 @@ export default function Projects({ data }) {
 
 function SingleProject({ project }) {
     return (
-        <Link href={`/projects/${project.name}`}>
+        <Link href={ `/projects/${project.name}` }>
             <ImageListItem
-                key={project.image}
-                style={{ cursor: "pointer" }}>
+                key={ project.image }
+                style={ { cursor: "pointer" } }>
                 <img
-                    src={project.image}
-                    alt={project.name}
+                    src={ project.image }
+                    alt={ project.name }
                     loading="lazy"
-                    style={{mixBlendMode : "multiply"}}
+                    style={ { mixBlendMode : "multiply" } }
                 />
                 <ImageListItemBar
-                    title={project.name}
-                    subtitle={project.role}
+                    title={ project.name }
+                    subtitle={ project.role }
                     actionIcon={
-                        <IconButton sx={{ color: 'rgba(255, 255, 255, 1)' }}>
+                        (<IconButton sx={ { color: "rgba(255, 255, 255, 1)" } }>
                             <ArrowCircleRight />
-                        </IconButton>
+                        </IconButton>)
                     }
                 />
             </ImageListItem>
