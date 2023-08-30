9f3c62ebeacba3c839a82779fae428ea05401d17
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2023-08-30 22:12:00 +0530

    Update myInterests.tsx

--- utils/components/pages/index/myInterests.tsx
+++ utils/components/pages/index/myInterests.tsx
@@ -17,7 +17,6 @@ export default function MyInterests({ data }) {
                     </>
                 ))
             }
-
             <CommonDivider />
         </>
     );
@@ -26,34 +25,33 @@ export default function MyInterests({ data }) {
 function Interest({ interest }) {
 
     return (
-        <>
-            <Stack spacing={ 1 }>
-                <Typography
-                    variant="h6"
-                    color="text.secondary"
-                    sx={ {
-                        fontWeight: "normal"
-                    } }>
-                    { interest.title }
-                </Typography>
-                <Stack direction="column" spacing={ 0.4 }>
-                    {
-                        interest.interests.map(
-                            (interestDetails) => (
-                                <Chip
-                                    key = { interestDetails.title }
-                                    avatar={ interestDetails.imageUrl 
+        <Stack spacing={ 1 }>
+            <Typography
+                variant="h6"
+                color="text.secondary"
+                sx={ {
+                    fontWeight: "normal"
+                } }>
+                { interest.title }
+            </Typography>
+            <Stack direction="column" spacing={ 0.5 }>
+                {
+                    interest.interests.map(
+                        (interestDetails) => (
+                            <Chip
+                                key = { interestDetails.title }
+                                avatar={ 
+                                    interestDetails.imageUrl 
                                         ? <Avatar src={ interestDetails.imageUrl } /> 
-                                        : null }
-                                    label={ interestDetails.title }
-                                    color={ interest.color } 
-                                />
-                            )
+                                        : null 
+                                }
+                                label={ interestDetails.title }
+                                color={ interest.color }
+                            />
                         )
-                    }
-                </Stack>
+                    )
+                }
             </Stack>
-
-        </>
+        </Stack>
     );
 }
