4a9e9bfe4e9010be505f7e6376ee0e1e8facb1c3
Achintha Isuru <achinthaisuru444@gmail.com>
2023-11-06 16:37:08 +0530

    UI Changes Init (#1)
    * UI changes initi
    
    * Revert "UI changes initi"
    
    This reverts commit afa3944ec12158288c15271f7302f5111ca94a66.
    
    * UI changes init
    
    This reverts commit 7c2a9a0e9090e70f0560fd74909e8309bc71c5d6.

--- utils/components/common/commonDivider.tsx
+++ utils/components/common/commonDivider.tsx
@@ -1,12 +1,18 @@
-import { Divider } from '@mui/material'
-import React from 'react'
+import { Divider } from "@mui/material";
+import React from "react";
 
 export default function CommonDivider() {
     return (
         <>
             <br />
-            <Divider style={{ position: "absolute", left: "0px", width: "100vw" }} />
+            <Divider 
+                style={ {
+                    left: "0px",
+                    position: "absolute", 
+                    width: "100vw" 
+                } } 
+            />
             <br />
         </>
-    )
+    );
 }
--- utils/components/common/mainGrid.tsx
+++ utils/components/common/mainGrid.tsx
@@ -1,5 +1,5 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import { Grid } from "@mui/material";
+import React from "react";
 
 export default function MainGrid(prop) {
 
@@ -7,24 +7,31 @@ export default function MainGrid(prop) {
 
     return (
         <div>
-            <Grid container sx={{
-                height: "100%"
-            }}>
-                <Grid item xs={0} sm={1} md={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Grid 
+                container 
+                sx={ {
+                    height: "100%"
+                } }
+            >
+                <Grid item xs={ 0 } sm={ 1 } md={ 2 } sx={ { display: { sm: "block" , xs: "none" } } }>
                 </Grid>
-                <Grid item xs={12} sm={10} md={8} sx={{
-                    '--Grid-borderWidth': '1px',
-                    borderLeft: 'var(--Grid-borderWidth) solid',
-                    borderRight: 'var(--Grid-borderWidth) solid',
-                    borderColor: 'divider',
-                    padding: "20px 40px",
-                    minHeight: "66vh"
-                }}>
-                    {children}
+                <Grid 
+                    item 
+                    xs={ 12 } 
+                    sm={ 10 } 
+                    md={ 8 } 
+                    sx={ {
+                        borderLeft: "var(--Grid-borderWidth) solid",
+                        borderRight: "var(--Grid-borderWidth) solid",
+                        minHeight: "66vh",
+                        padding: "20px 40px"
+                    } }
+                >
+                    { children }
                 </Grid>
-                <Grid item xs={0} sm={1} md={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+                <Grid item xs={ 0 } sm={ 1 } md={ 2 } sx={ { display: { sm: "block", xs: "none" } } }>
                 </Grid>
             </Grid>
         </div>
-    )
+    );
 }
